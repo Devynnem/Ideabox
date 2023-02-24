5cf890e32d231eb7a638d0d4eb84f5b3ae547e40
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-02-23 19:44:06 -0500

    feat: Add functionality to empty input save button and updated images

--- main.js
+++ main.js
@@ -14,13 +14,26 @@ var savedIdeas= [] // this is what we will push saved ideas into
 // if (userTitle.value === '' || userBody.value === '') {
     // classList.add('.save-button2')
     //}
-    userSaveBtn.addEventListener('mouseover', changeButtonColor)
-    
-    
+    userTitle.addEventListener('input', changeButtonColor)
+    userBody.addEventListener('input',changeButtonColor)
     userSaveBtn.addEventListener('click', createIdea,)
+    changeButtonColor();
+
+    function changeButtonColor() {
+        // event.preventDefault()
+     if (!userTitle.value || !userBody.value) {
+         userSaveBtn.classList.remove('save-button-able');
+         userSaveBtn.disabled = true;
+     } else if (userTitle.value && userBody.value){
+        userSaveBtn.classList.add('save-button-able');
+        userSaveBtn.disabled =false;
+     }
+     }
+
     function saveUserIdea() {
         savedIdeas.push(currentIdea)
     } 
+    
     function clearInputs() {
         userTitle.value = '';
         userBody.value = '';
@@ -35,11 +48,11 @@ var savedIdeas= [] // this is what we will push saved ideas into
         ideaBoxes.innerHTML += `<section class="idea-container">
         <section class="top-margin">
         <button class="star-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.06 15.65"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Artboard 29</title><g id="background"><polygon class="cls-1" points="8.02 2.48 9.64 5.77 13.27 6.29 10.64 8.85 11.26 12.47 8.02 10.76 4.77 12.47 5.39 8.85 2.76 6.29 6.39 5.77 8.02 2.48"/></g></svg>
-        <svg class="hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.93 16.49"><defs><style>.cls-1{fill:#f16139;}</style></defs><title>Artboard 30</title><g id="background"><polygon class="cls-1" points="8.28 2.85 9.91 6.13 13.53 6.66 10.91 9.22 11.53 12.83 8.28 11.13 5.04 12.83 5.66 9.22 3.03 6.66 6.66 6.13 8.28 2.85"/></g></svg>
+        <img class="star-icon" id="star-icon" src="assets/star.svg" alt="blank favorite icon">
+        <img class="star-icon-favorited hidden" id="star-icon-favorited" src="assets/star-active.svg" alt="favorite icon active">
         </button>
         <button class="x-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.18 17.72"><defs><style>.cls-1{fill:"#fff";}</style></defs><title>Artboard 31</title><g id="background"><polygon class="cls-1" points="13.13 5.87 12.43 5.16 8.97 8.61 5.53 5.16 4.82 5.87 8.27 9.32 4.82 12.77 5.53 13.48 8.97 10.03 12.43 13.48 13.13 12.77 9.68 9.32 13.13 5.87"/></g></svg>
+        <img class="x-icon" id="x-icon" src="assets/delete.svg" alt="delete icon">
         </button>
         </section>
         <section class="idea-contents">
@@ -48,23 +61,21 @@ var savedIdeas= [] // this is what we will push saved ideas into
         </section>
         <section class="bottom-bar">
         <button class="plus-button">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.71 19.71"><defs><style>.cls-1{fill:#fff;stroke:#353567;stroke-miterlimit:10;}.cls-2{fill:#353567;}</style></defs><title>abacus-v1Artboard 41</title><g id="background"><circle class="cls-1" cx="9.76" cy="9.76" r="5.58"/><polygon class="cls-2" points="5.65 9.37 5.65 10.14 9.37 10.14 9.37 13.86 10.14 13.86 10.14 10.14 13.86 10.14 13.86 9.37 10.14 9.37 10.14 5.65 9.37 5.65 9.37 9.37 5.65 9.37"/></g></svg>
+        <img class="plus-icon" id="plus-icon" src="assets/comment.svg" alt="comment icon">
         </button>
         <h3 id="idea-card-comment">Comment</h3>
         </section>
         </section>`
-        clearInputs()
-        
-    }
-    
-    
-    // userSaveBtn.disabled = true; //setting button state to disabled
-    
-    function changeButtonColor() {
-        userSaveBtn.disabled = true
-     if (userTitle.value === '' || userBody.value === '') {
-         userSaveBtn.style.backgroundColor = '#ffffff';
-         userSaveBtn.disabled = true
-     }
+        clearInputs() 
+        changeButtonColor()
     }
 
+     userSaveBtn.disabled = true;
+    
+    
+    
+    
+    
+    
+    
+    
